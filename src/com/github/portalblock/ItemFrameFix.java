748de199e54f585951c6fa342366091c73912da6
portalBlock <portalBlock@users.github.com>
2013-12-23 20:27:00 -0600

    My 4th try at this stuff!

---
+++ src/com/github/portalblock/ItemFrameFix.java
@@ -0,0 +1,14 @@
+package com.github.portalblock;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by portalBlock on 23/12/13.
+ */
+public class ItemFrameFix extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable(){
+
+    }
+}
