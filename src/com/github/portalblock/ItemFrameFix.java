89a2fa0cac36741b231884dcb4ea4eb9624364ea
portalBlock <portalBlock@users.github.com>
2013-12-23 20:46:37 -0600

    Hopefully made the code work.

--- src/com/github/portalblock/ItemFrameFix.java
+++ src/com/github/portalblock/ItemFrameFix.java
@@ -1,9 +1,11 @@
 package com.github.portalblock;
 
+import org.bukkit.GameMode;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.hanging.HangingBreakByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -18,10 +20,13 @@ public class ItemFrameFix extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void hBE(HangingBreakByEntityEvent e){
-       Entity en = e.getRemover();
+    public void hBE(EntityDamageByEntityEvent e){
+       Entity en = e.getEntity();
        if(en instanceof Player){
-           e.setCancelled(true);
+           Player p = (Player)en;
+           if(p.getGameMode() != GameMode.CREATIVE){
+                e.setCancelled(true);
+           }
        }
     }
 }
